d3f1ebd8650f55bec777fa0ede983e22ad3b4e56
Thuylan <20152107@student.hust.edu.vn>
2019-04-06 23:23:05 +0700

    Create dish class

---
+++ CookWeb/CookWeb/Models/Dish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CookWeb.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ApplicationUser Author { get; set; }
+    }
+}
