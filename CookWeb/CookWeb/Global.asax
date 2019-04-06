310365fb3a2bb4feabc0009f5a7a6b665f2a6796
Thuylan <20152107@student.hust.edu.vn>
2019-04-06 23:13:26 +0700

    Init project CookWeb

---
+++ CookWeb/CookWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CookWeb.MvcApplication" Language="C#" %>
---
+++ CookWeb/CookWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CookWeb.Startup))]
+namespace CookWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
